365683b846abb36ece559bc3f17b2031a7e09b4c
Pranay Anand <pranayanand123@gmail.com>
2018-07-29 13:10:05 +0530

    Add All Files

---
+++ app/src/main/java/com/example/pranay/ecellgallery/Image.java
@@ -0,0 +1,18 @@
+package com.example.pranay.ecellgallery;
+
+/**
+ * Created by pranay on 28-07-2018.
+ */
+
+public class Image {
+
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ app/src/main/java/com/example/pranay/ecellgallery/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.pranay.ecellgallery;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.AssetManager;
+import com.example.pranay.ecellgallery.Image;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Image> images;
+    private ProgressDialog pDialog;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
+    private static final String TAG = "JSON";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        pDialog = new ProgressDialog(this);
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(getApplicationContext(), images);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.addOnItemTouchListener(new GalleryAdapter.RecyclerTouchListener(getApplicationContext(), recyclerView, new GalleryAdapter.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("images", images);
+                bundle.putInt("position", position);
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                SlideshowDialogFragment newFragment = SlideshowDialogFragment.newInstance();
+                newFragment.setArguments(bundle);
+                newFragment.show(ft, "slideshow");
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+        fetchImages();
+
+
+
+    }
+    private void fetchImages() {
+        images.clear();
+        try {
+            JSONArray array = new JSONArray(loadJSONFromAsset(getApplicationContext(), "images.json"));
+            for(int i=0;i<array.length();i++){
+                JSONObject object = array.getJSONObject(i);
+                Image image = new Image();
+                image.setImageUrl(object.getString("url"));
+                images.add(image);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mAdapter.notifyDataSetChanged();
+
+
+    }
+    private static String loadJSONFromAsset(Context context, String jsonFileName) {
+        String json = null;
+        InputStream is=null;
+        try {
+            AssetManager manager = context.getAssets();
+            Log.d(TAG,"path "+jsonFileName);
+            is = manager.open(jsonFileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
